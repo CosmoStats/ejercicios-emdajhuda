5ad4a0baa658904a8a2f1d982f2cbd1a2010c1c3
Emmanuel Juda <juda@MacBook-Air-de-Administrador.local>
2026-06-18 01:06:08 -0600

    Se añadieron los calculos para los modelos omegaklcdm, wcdm y uno de los analisis con w0wacdm

--- proyecto_final/graph_desibao.py
+++ proyecto_final/graph_desibao.py
@@ -1,7 +1,7 @@
 from getdist import loadMCSamples, plots
 
-samples = loadMCSamples('/Users/juda/lab_alma/proyecto_final/chains/proyecto_only_desibao_plano')
+samples = loadMCSamples('/Users/juda/lab_alma/proyecto_final/chains/proyecto_only_desibao/proyecto_only_desibao')
 
 g = plots.get_subplot_plotter()
 g.triangle_plot(samples, ['H0', 'omega_b', 'omega_cdm'], filled=True)
-g.export('triangle_plot_only_desibao_plano.png')
+g.export('triangle_plot_only_desibao.png')
